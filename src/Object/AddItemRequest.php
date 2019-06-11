827e63828438f96314a82b8b0e3edefca4491537
Tomas <krizek@vergilio.cz>
2019-06-11 14:57:06 +0200

    fix kvůli discount-percent u polozky objednavky

--- src/Object/AddItemRequest.php
+++ src/Object/AddItemRequest.php
@@ -17,4 +17,35 @@ class AddItemRequest extends ObjectBase
 
     /** @var string */
     public $webhook;
+
+    // ugly fix
+    public function __toString()
+    {
+        $arr = [];
+        foreach ($this as $k => $v) {
+            if (!(null === $v)) {
+                $field = $this->mapPropertyToField($k);
+
+                if ($k === 'items') {
+                    $items = [];
+                    foreach ($v as $item) {
+                        $itemsArr = [];
+                        foreach ($item as $kk => $vv) {
+                            if (!(null === $vv)) {
+                                $subField = $this->mapPropertyToField($kk);
+                                $itemsArr[$subField] = $vv;
+                            }
+                        }
+                        $items[] = $itemsArr;
+                    }
+                    $arr[$field] = $items;
+
+                } else {
+                    $arr[$field] = $v;
+                }
+            }
+        }
+
+        return json_encode($arr);
+    }
 }
